cb8adef179fefb3dbd846e92ffc594283aa37646
Programmer2011bird <maysamshaker7@gmail.com>
2024-12-14 23:49:30 +0330

    returning file content

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+## SERVER ##
+
+import hashlib
+from os.path import exists
 import socket
 
 
-class websocket:
+def return_file_content(fileName:str) -> str | bytes:
+    if not exists(fileName):
+        return b"File Not Found"
+
+    with open(f"{fileName}", "r+") as file:
+        CONTENT: str = str(file.read())
+
+    return CONTENT
+
+
+class server:
     def __init__(self, HOST:str="127.0.0.1", PORT:int=8080) -> None:
         self.HOST: str = HOST
         self.PORT: int = PORT
@@ -9,13 +23,22 @@ class websocket:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.SOCKET:
             self.SOCKET.bind((HOST, PORT))
             self.SOCKET.listen()
+            
+            while True:
+                connection, address = self.SOCKET.accept()
 
-            connection, address = self.SOCKET.accept()
-
-            fileName: str = str(connection.recv(1024).decode()).split("\n")[-1]
-
-            connection.close()
+                fileName: str = str(connection.recv(1024).decode()).split("\n")[-1]
+                content: str | bytes = return_file_content(fileName)
+    
+                if type(content) == bytes and content.decode() == "File Not Found":
+                    print(f"[{address[0]}:{address[1]}] {fileName} -> Status : ERROR > File Not Found")
+                    connection.close()
+    
+                else:
+                    connection.sendall(content.encode())
+                    print(f"[{address[0]}:{address[1]}] {fileName} -> Status : OK")
+                    connection.close()
 
 
 if __name__ == "__main__":
-    WEBSOCKET: websocket = websocket()
+    WEBSOCKET: server = server()
